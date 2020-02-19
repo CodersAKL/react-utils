63f23c5c9153315c8c7d1fdcc50345d0b2dd1f8c
Vytenis Kuciauskas <projektas@gmail.com>
2020-02-19 16:30:42 +0200

    Display converters only on js files

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "react-toolkit" is now active!');
 
     const commandExtractStyle = vscode.commands.registerCommand('react-toolkit.extractStyle', extractStyle);
-    const commandReadFile = vscode.commands.registerCommand('react-toolkit.readFile', convertFileToTypescript);
+    const commandReadFile = vscode.commands.registerCommand('react-toolkit.convertToTS', convertFileToTypescript);
     const commandCjsToEsm = vscode.commands.registerCommand('react-toolkit.convertCjsToEsm', convertCjsToEsm);
 
     context.subscriptions.push(commandExtractStyle);
